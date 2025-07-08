211690fb21019c8e4576e8456d60f91a9dc574c2
bing <taaaaaang@gmail.com>
2025-07-08 11:48:35 -0600

    添加设置文件。尝试修复 ARM 版本编译问题。

---
+++ env.bat
@@ -0,0 +1,26 @@
+rem Try to set up the Visual Studio developer environment.
+if defined VS2022INSTALLDIR (
+    call "%VS2022INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if defined VSINSTALLDIR (
+    call "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
+
+rem Customize your build environment and save the modified copy to env.bat
+
+set WEASEL_ROOT=%CD%
+
+rem REQUIRED: path to Boost source directory
+if not defined BOOST_ROOT set BOOST_ROOT=C:\local\boost_1_78_0
+
+rem OPTIONAL: architecture, Visual Studio version and platform toolset
+rem set ARCH=Win32
+set BJAM_TOOLSET=msvc-14.3
+set CMAKE_GENERATOR="Visual Studio 17 2022"
+set PLATFORM_TOOLSET=v143
+
+rem OPTIONAL: path to additional build tools
+set DEVTOOLS_PATH=%ProgramFiles%\Git\usr\bin;%ProgramFiles%\Git\cmd;%ProgramFiles%\CMake\bin;
